d376dfdfd2522688ee4c9768d90a1dea54969ea9
Ayush Bhavsar <ayush21bhavsar@gmail.com>
2024-05-14 13:05:55 +0530

    Deploy test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function App() {
   return (
+    <div className='h-[100vh] w-[100vw] bg-black ' >
     <h1 className='bg-black text-white ' > HI MOM </h1>
+    </div>
   )
 }
 
